1607f0892f674c0b765d1183e6479c09af99558e
vickohh <60358928+vickohh@users.noreply.github.com>
2020-09-13 01:18:18 -0500

    set primary Categorias bean

--- src/main/java/com/vck/empleos/service/db/CategoriasServiceJPA.java
+++ src/main/java/com/vck/empleos/service/db/CategoriasServiceJPA.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.vck.empleos.model.Categoria;
@@ -11,6 +12,7 @@ import com.vck.empleos.repository.CategoriasRepository;
 import com.vck.empleos.service.ICategoriasService;
 
 @Service
+@Primary
 public class CategoriasServiceJPA implements ICategoriasService {
 	
 	@Autowired
