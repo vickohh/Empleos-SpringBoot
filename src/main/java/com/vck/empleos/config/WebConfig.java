784ab7c4529412aa6061d8e72829dcb24e57d0c9
vickohh <60358928+vickohh@users.noreply.github.com>
2020-09-14 23:15:31 -0500

    fixes

--- src/main/java/com/vck/empleos/config/WebConfig.java
+++ src/main/java/com/vck/empleos/config/WebConfig.java
@@ -1,14 +1,21 @@
 package com.vck.empleos.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+// aqui determinamos que las imagenes se  reflejen en el path logos, tambien que las imagenes estaticas se autocarguen despues de agregarla en el formulario
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	
+	@Value("${empleosapp.ruta.imagenes}")
+	private String rutaimagenes;
+	
+	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/CodeBase/SpringBoot/Empleos/src/main/resources/static/images/");		
+		registry.addResourceHandler("/logos/**").addResourceLocations("file:"+rutaimagenes);		
 	}
 	
 
